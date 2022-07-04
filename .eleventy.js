b24ce9bdde1a2315bcc15814308416531a00608a
Daniel Zenzes <daniel@zenzes.me>
2022-07-04 07:03:55 +0200

    add tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
+const tailwind = require("tailwindcss");
+const postCss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+const postcssFilter = (cssCode, done) => {
+  postCss([
+    tailwind(require("./tailwind.config")),
+    autoprefixer(),
+    cssnano({ preset: "default" }),
+  ])
+    .process(cssCode, {
+      from: "./src/_includes/styles/tailwind.css",
+    })
+    .then(
+      (r) => done(null, r.css),
+      (e) => done(e, null)
+    );
+};
+
 module.exports = function (config) {
-  config.addPassthroughCopy("src/style.css");
+  config.addWatchTarget("./src/_includes/styles/tailwind.css");
+  config.addNunjucksAsyncFilter("postcss", postcssFilter);
   return {
     passthroughFileCopy: true,
     dir: {
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["src/**/*.njk"],
+  safelist: [],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        change: "transparent",
+      },
+    },
+  },
+  plugins: [],
+};
